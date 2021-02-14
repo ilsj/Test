e3fa6547ff63fe9306cfe1560793b46492997fb2
ilsj <uralrek@mail.ru>
2021-02-14 20:11:04 +0500

    Del  js/art-proxy-auto-config.js

--- js/art-proxy-auto-config.js
+++
@@ -1,9 +0,0 @@
-function FindProxyForURL(url, host) {
-
-    // use proxy for specific domains
-    if (host ==  "94.79.54.123")
-        return "PROXY 185.185.71.8:11488";
-
-    // by default use no proxy
-    return "DIRECT";
-}
